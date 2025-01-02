bceaa883404d7b0f517cdc784fed7d825a957521
I <1091761+wx257osn2@users.noreply.github.com>
2025-01-02 15:38:05 +0900

    name generated file explicitly due to prevent accidents involving mistaken files

--- build.bash
+++ build.bash
@@ -4,10 +4,10 @@
 here="$(realpath $(dirname ${BASH_SOURCE:-$0}))"
 
 generate_def() {
-  cat ${here}/${1}.in > ${here}/${1}
-  echo "PS1=\"Singularity ${PS1}\"" >> ${here}/${1}
-  echo 'EOF' >> ${here}/${1}
-  echo '  chmod +x $CUSTOM_ENV' >> ${here}/${1}
+  cat ${here}/${1}.def.in > ${here}/.generated.def
+  echo "PS1=\"Singularity ${PS1}\"" >> ${here}/.generated.def
+  echo 'EOF' >> ${here}/.generated.def
+  echo '  chmod +x $CUSTOM_ENV' >> ${here}/.generated.def
 }
 
 unset XDG_RUNTIME_DIR
@@ -22,10 +22,10 @@ if which docker > /dev/null && [[ $docker_user = 0 ]] || [[ $EUID = 0 ]] ; then
   mkdir -p ${here}/empty
   DOCKER_BUILDKIT=1 docker build -t cxx:latest -f ${here}/Dockerfile ${here}/empty
   rmdir ${here}/empty
-  generate_def with_docker.def
-  singularity build ${here}/cxx.sif ${here}/with_docker.def
+  generate_def with_docker
+  singularity build ${here}/cxx.sif ${here}/.generated.def
 else
   # docker is unavailable, so use singularity directly
-  generate_def standalone.def
-  singularity build --fakeroot ${here}/cxx.sif ${here}/standalone.def
+  generate_def standalone
+  singularity build --fakeroot ${here}/cxx.sif ${here}/.generated.def
 fi
