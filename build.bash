8711bb262ad541b9d2eab1a6df36189052113d33
I <1091761+wx257osn2@users.noreply.github.com>
2025-01-06 19:19:34 +0900

    add default prompt

--- build.bash
+++ build.bash
@@ -6,7 +6,7 @@ here="$(realpath $(dirname ${BASH_SOURCE:-$0}))"
 
 generate_def() {
   cat ${here}/${1}.def.in > ${here}/.generated.def
-  echo "PS1=\"\e[36m(cxx singularity)\e[0m ${PS1}\"" >> ${here}/.generated.def
+  echo "PS1=\"\e[36m(cxx singularity)\e[0m ${PS1:-${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ }\"" >> ${here}/.generated.def
   echo 'EOF' >> ${here}/.generated.def
   echo '  chmod +x $CUSTOM_ENV' >> ${here}/.generated.def
 }
