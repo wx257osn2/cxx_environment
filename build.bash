faf7ccbee1d6a4c2d81e90976eed9d088775a95c
I <1091761+wx257osn2@users.noreply.github.com>
2025-09-23 07:39:14 +0900

    fix prompt to work properly with readline

--- build.bash
+++ build.bash
@@ -16,7 +16,7 @@ fi
 
 generate_def() {
   cat ${here}/${1}.def.in > ${here}/.generated.def
-  echo "PS1=\"\e[36m(cxx singularity)\e[0m ${PS1:-${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ }\"" >> ${here}/.generated.def
+  echo "PS1=\"\[\e[36m\](cxx singularity)\[\e[0m\] ${PS1:-${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ }\"" >> ${here}/.generated.def
   echo 'EOF' >> ${here}/.generated.def
   echo '  chmod +x $CUSTOM_ENV' >> ${here}/.generated.def
 }
