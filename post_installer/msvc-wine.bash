8082e1b18c44de7e1cec14e2fdfe49224ba995db
I <1091761+wx257osn2@users.noreply.github.com>
2026-07-03 07:06:07 +0900

    bump up msvc-wine

--- post_installer/msvc-wine.bash
+++ post_installer/msvc-wine.bash
@@ -21,17 +21,13 @@ pushd _msvc-wine
 trap 'popd; rm -rf _msvc-wine' EXIT
 git init
 git remote add origin https://github.com/mstorsjo/msvc-wine
-git fetch --depth 1 origin 32b504c63b869681cda6824a20e30b74cb718432
+git fetch --depth 1 origin 514f8ea34842cd6d831804d0e9658d3a32870ae1
 git reset --hard FETCH_HEAD
 ./vsdownload.py --major ${major} ${preview} --accept-license --only-host --dest ${dest}
 rm -r ${dest}/VC/Tools/MSVC/*
 ./vsdownload.py --major ${major} --msvc-version ${version} ${preview} --accept-license --only-host --dest ${dest}
 echo 'REDISTRIBUTABLE_PATH=$(echo ${BASE_UNIX}/VC/Redist/MSVC/${MSVCVER%.*}*)' >> wrappers/msvcenv.sh
 echo 'export WINEPATH="${WINEPATH};z:${REDISTRIBUTABLE_PATH//\//\\}\\debug_nonredist\\x64\\Microsoft.VC143.DebugCRT;${SDKBINDIR//\//\\}\\ucrt"' >> wrappers/msvcenv.sh
-if [ ${major} -eq 18 ]; then
-  sed "s/170/${major}0/g" -i wrappers/msbuild
-  sed "s/143/145/g" -i wrappers/msvcenv.sh
-fi
 ./install.sh ${dest}
 
 wine_version=$(wine --version | sed -e 's/wine-\([0-9.]*\) (.*)/\1/')
