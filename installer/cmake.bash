a8cc91d598dc9002be71ab16a6b335e5d0d3f35b
I <1091761+wx257osn2@users.noreply.github.com>
2026-09-24 15:52:26 +0900

    download archive directly

--- installer/cmake.bash
+++ installer/cmake.bash
@@ -5,7 +5,5 @@ set -euo pipefail
 CMAKE_VERSION=${1}
 ARCH=$(uname -m)  # It assumes to be x86_64 or aarch64
 
-curl -sSLO https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-${ARCH}.sh
 mkdir -p /opt/cmake-${CMAKE_VERSION}
-bash cmake-${CMAKE_VERSION}-linux-${ARCH}.sh --prefix=/opt/cmake-${CMAKE_VERSION} --skip-license
-rm cmake-${CMAKE_VERSION}-linux-${ARCH}.sh
+curl -sSL https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-${ARCH}.tar.gz | tar -xzf - -C /opt/cmake-${CMAKE_VERSION} --strip-components=1
